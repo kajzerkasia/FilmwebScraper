05d4935a3dc94988e9217d5445c0b7b661ef87b1
Kasia <kajzerkatarzyna1@gmail.com>
2023-07-09 14:12:26 +0200

    Created an array of objects with names and URLs for VOD services with movies from the current year.

---
+++ index.js
@@ -0,0 +1,8 @@
+const currentYear = new Date().getFullYear();
+
+const vodServices = [
+    {name: 'Netflix', url: `https://www.filmweb.pl/ranking/vod/netflix/film/${currentYear}`},
+    {name: 'HBO Max', url: `https://www.filmweb.pl/ranking/vod/hbo_max/film/${currentYear}`},
+    {name: 'Canal+', url: `https://www.filmweb.pl/ranking/vod/canal_plus/film/${currentYear}`},
+    {name: 'Disney', url: `https://www.filmweb.pl/ranking/vod/disney/film/${currentYear}`}
+];
